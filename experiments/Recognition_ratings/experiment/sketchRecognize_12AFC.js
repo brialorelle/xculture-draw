af376937cbf5b777821e04f9c49af976e91a81f2
Bria Long <brialorelle@gmail.com>
2021-04-28 15:55:03 -0700

    add recognition ratings experiment

---
+++ experiments/Recognition_ratings/experiment/sketchRecognize_12AFC.js
@@ -0,0 +1,249 @@
+// ############################ Helper functions ##############################
+
+// Shows slides. We're using jQuery here - the **$** is the jQuery selector function, which takes as input either a DOM element or a CSS selector string.
+function showSlide(id) {
+	// Hide all slides
+	$(".slide").hide();
+	// Show just the slide we want to show
+	$("#"+id).show();
+}
+
+
+// Get random integers.
+// When called with no arguments, it returns either 0 or 1. When called with one argument, *a*, it returns a number in {*0, 1, ..., a-1*}. When called with two arguments, *a* and *b*, returns a random value in {*a*, *a + 1*, ... , *b*}.
+function random(a,b) {
+	if (typeof b == "undefined") {
+		a = a || 2;
+		return Math.floor(Math.random()*a);
+	} else {
+		return Math.floor(Math.random()*(b-a+1)) + a;
+	}
+}
+
+// Add a random selection function to all arrays (e.g., <code>[4,8,7].random()</code> could return 4, 8, or 7). This is useful for condition randomization.
+Array.prototype.random = function() {
+  return this[random(this.length)];
+}
+
+// shuffle function - from stackoverflow?
+// shuffle ordering of argument array -- are we missing a parenthesis?
+function shuffle (a) 
+{ 
+    var o = [];
+    
+    for (var i=0; i < a.length; i++) {
+	o[i] = a[i];
+    }
+    
+    for (var j, x, i = o.length;
+	 i;
+	 j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);	
+    return o;
+}
+
+function shuffle_mult() {
+    var length0 = 0,
+        length = arguments.length,
+        i,
+        j,
+        rnd,
+        tmp;
+
+    for (i = 0; i < length; i += 1) {
+        if ({}.toString.call(arguments[i]) !== "[object Array]") {
+            throw new TypeError("Argument is not an array.");
+        }
+
+        if (i === 0) {
+            length0 = arguments[0].length;
+        }
+
+        if (length0 !== arguments[i].length) {
+            throw new RangeError("Array lengths do not match.");
+        }
+    }
+
+
+    for (i = 0; i < length0; i += 1) {
+        rnd = Math.floor(Math.random() * i);
+        for (j = 0; j < length; j += 1) {
+            tmp = arguments[j][i];
+            arguments[j][i] = arguments[j][rnd];
+            arguments[j][rnd] = tmp;
+        }
+    }
+}
+
+// ######################## Configuration settings ############################
+
+// connect to node.js
+socket = io.connect();
+
+//set up trials from csv
+$(document).ready(function() {
+
+$.ajax({
+        type: "GET",
+        url: "recognition_rating_chunks/chunk_1_compiled_dataset.csv",
+        dataType: "text",
+        success: function(data) {
+            results = Papa.parse(data);
+            
+            imgArray = new Array();
+            //set up image names
+            for (i = 0; i < results.data.length; i++) {
+                var imageName= results.data[i][8]; //starts i at 1 to get rid of header
+                var imageCategory=results.data[i][3]
+                imgArray[i] = new Image();
+                imgArray[i].src = ['object_drawings/' + imageName ];
+                imgArray[i].name = imageCategory;
+            }                 
+               
+        
+    trials = []
+    numTrialsExperiment = imgArray.length;    
+
+    try{
+        url = window.location.href
+        var this_sub_id = url.substring(url.lastIndexOf('PROLIFIC_PID')+13, url.lastIndexOf('PROLIFIC_PID') + 36);
+        console.log(this_sub_id)
+    }
+    catch(err){
+        var this_sub_id = random(10000000000)
+        console.log(this_sub_id)
+    }
+    
+    for (i = 0; i < numTrialsExperiment; i++) {
+        trial = {
+            thisImageName: imgArray[i].src,
+            slide: "recognitionRatings",
+            sub_id: this_sub_id,
+        }
+        trials.push(trial);
+    }
+    console.log(trials.length)
+    
+    // shuffle actual trials
+    trials=shuffle(trials);
+    console.log(trials.length)
+
+}
+}); // ajax 
+}); // document ready
+
+var availableTags = ["watch","bike","chair","car","tree","rabbit","house","cup","hat","cat","bird","airplane"]
+$("#recognitionInput").autocomplete({
+            source: availableTags,
+            change: function (event, ui) {
+                if(!ui.item){
+                    //http://api.jqueryui.com/autocomplete/#event-change -
+                    // The item selected from the menu, if any. Otherwise the property is null
+                    //so clear the item for force selection
+                    $("#recognitionInput").val("");
+                }
+
+            }
+
+        });
+
+
+showSlide("instructions"); // Show the instructions slide -- this is what we want subjects to see first.
+
+// ############################## The main event ##############################
+var experiment = {
+
+	// The object to be submitted.
+	data: {
+        sub_id: [],
+        guessed_category: [],
+        dbname:'devphotodraw_recognition',
+        colname: 'batched_12afc', 
+        trial_type: [].
+		comments: [],
+	},
+	// end the experiment
+	end: function() {
+		showSlide("finished");
+		setTimeout(function() {
+            socket.emit('current_data', experiment.data);
+            window.location.href="https://app.prolific.co/submissions/complete?cc=3769E361"
+		}, 1500);
+	},
+
+
+// LOG RESPONSE
+    log_response: function() {
+
+        var response_logged = false;
+        var input = document.getElementById("recognitionInput");
+        var response = input.value;
+
+        // if there is something in the response, log it
+        if (input && response) {
+            response_logged = true;
+            
+            readable_date = new Date();
+            trial_data =  {
+                date: readable_date,
+                sub_id: trials[0].sub_id,
+                dataType: 'recognition_rating',
+                dbname:'devphotodraw_recognition',
+                colname: 'batched_12afc', 
+                guessed_category: response,
+                imageCategory: document.getElementById("imagePlaceholder").name,
+                imageName: document.getElementById("imagePlaceholder").src,
+            }
+
+            socket.emit('current_data', trial_data);
+            experiment.next();
+            $("#recognitionInput").val(""); // clear value
+            
+        } else {
+            $("#testMessage_att").html('<font color="red">' + 
+            'Please make a response!' + 
+             '</font>');   
+        }
+    },
+	
+	// The work horse of the sequence - what to do on every trial.
+	next: function() {
+
+		// Allow experiment to start if it's a turk worker OR if it's a test run
+		if (window.self == window.top) {
+		$("#testMessage_att").html(''); //clear test message
+		$("#testMessage_uptake").html(''); 
+
+
+		$("#progress").attr("style","width:" +
+			    String(100 * (1 - (trials.length)/numTrialsExperiment)) + "%")
+			// Get the current trial - <code>shift()</code> removes the first element
+			// select from our scales array and stop exp after we've exhausted all the domains
+			var trial_info = trials.shift();
+
+			//If the current trial is undefined, call the end function.
+
+			if (typeof trial_info == "undefined") {
+				return experiment.debriefing();
+			}
+            // check which trial type you're in and display correct slide
+            if (trial_info.slide == "recognitionRatings") {
+                document.getElementById("imagePlaceholder").src = trial_info.thisImageName;
+                showSlide("recognitionRatings"); //display slide
+                experiment.data.imageName.push(trial_info.thisImageName);
+                }
+        experiment.data.trial_type.push(trial_info.slide);
+		}
+	},
+
+	//	go to debriefing slide
+    debriefing: function() {
+        showSlide("debriefing");
+    },
+
+// submitcomments function
+    submit_comments: function() {
+        experiment.data.comments.push(document.getElementById("comments").value);
+        experiment.end();
+    }
+}
+

a6205c3c0265c2f300178951bc82a6de37a21fe7
Bria Long <brialorelle@gmail.com>
2021-06-18 13:35:53 -0700

    batch 18 production

--- experiments/Recognition_ratings/experiment/sketchRecognize_12AFC.js
+++ experiments/Recognition_ratings/experiment/sketchRecognize_12AFC.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
 
 $.ajax({
         type: "GET",
-        url: "recognition_rating_chunks/chunk_17_compiled_dataset.csv",
+        url: "recognition_rating_chunks/chunk_18_compiled_dataset.csv",
         dataType: "text",
         success: function(data) {
             results = Papa.parse(data);
@@ -99,7 +99,7 @@ $.ajax({
                
     
     //global variable 
-    this_version = 'batch17_production_june2021'
+    this_version = 'batch18_production_june2021'
     //
     trials = []
     numTrialsExperiment = imgArray.length;    
